8091687aaa2de8b7f7c2e860b16c4e9ac40a91af
ankuraditya13 <70592234+ankuraditya13@users.noreply.github.com>
2020-09-03 01:59:39 +0530

    Update Assign1_Q55 (2).py

--- Assign1_Q55 (2).py
+++ Assign1_Q55 (2).py
@@ -1,9 +1,23 @@
-a = 1.0
-b = 3.0
-c = 7.0
-x1 = 3.0
-y1 = 8.0
-x= ((2*a*c)-(2*b*a*y1)-(x1*(a*a-b*b)))/(a*a+b*b); 
-y= ((a*a-b*b)*y1-2*a*b*x1+2*b*c)/(a*a+b*b);  
-print("Image of the point (" + str(x1) + ", " + str(y1) + ") is")
-print( "(" + str(x) + ", " + str(y) + ")" )
+a=1;
+b=3;
+c=7;
+x1=3;
+y1=8;
+
+import numpy as np
+
+m = np.array([[b], [-a]]);
+n = np.array([[a],[b]]);
+P = np.array([[x1],[y1]]);
+
+m1 = m.transpose()
+n1 = n.transpose()
+
+den = (m1 @ m) + (n1 @ n);
+num = ( (m @ m1) - (n @ n1) ) @ P;
+
+n11= pow(a,2)+pow(b,2);
+
+x = (num/den) + (c/n11) * n;
+R=2*x;
+print(R)
