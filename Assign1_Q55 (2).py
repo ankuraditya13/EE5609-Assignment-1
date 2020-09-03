905d60a3baa6857443bed3c49763fffeac8f9439
ankuraditya13 <70592234+ankuraditya13@users.noreply.github.com>
2020-09-03 22:41:33 +0530

    Update Assign1_Q55 (2).py

--- Assign1_Q55 (2).py
+++ Assign1_Q55 (2).py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 a=1;
 b=3;
 c=7;
@@ -20,4 +21,26 @@ n11= pow(a,2)+pow(b,2);
 
 x = (num/den) + (c/n11) * n;
 R=2*x;
-print(R)
+
+x = np.linspace(-15,15,500)
+
+e = (c-a*x)/b;
+plt.plot(x, e)
+
+f = P[1] + ( (x-P[0]) * ((R[1]-P[1])/(R[0]-P[0])))
+
+plt.annotate("(3,8)", (P[0], P[1]))
+plt.annotate("(-1,-4)", (R[0], R[1]))
+plt.grid()
+
+plt.plot(x, f)
+
+plt.xlabel('x', color='#1C2833')
+plt.ylabel('y', color='#1C2833')
+
+
+plt.xlim(-15,15)
+plt.ylim(-15,15)
+
+plt.show()
+plt.savefig("image_of_point_in_2D_line.png", bbox_inches='tight')
